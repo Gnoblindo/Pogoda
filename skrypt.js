8d6e9a8b54f1cce3ac0ad1123db33b645f30107c
Gnoblindo <szymon.przybyla2010@gmail.com>
2024-11-24 19:12:33 +0100

    Trwa naprawa

--- skrypt.js
+++ skrypt.js
@@ -1,8 +1,5 @@
-var przy1 = document.getElementById('Pogoda');
-var Wynik = document.getElementById("Wynik")
-przy1.addEventListener("click", fetchData());
 function fetchData() {
-	fetch('http://api.weatherapi.com/v1/search.json?key=<12c455b685114ca09cd163336242411>&q=Zabrze', {
+	fetch('http://api.weatherapi.com/v1/current.json?key=12c455b685114ca09cd163336242411&q=Zabrze&lang=pl', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
@@ -15,9 +12,12 @@ function fetchData() {
           return response.json();
         })
         .then(data => {
-          Wynik.innerHTML(data);
+          console.log(JSON.stringify(data));
+          document.getElementById("Wynik1").innerHTML = "Temperatura: " + data.current.temp_c + "°C";
+          document.getElementById("Wynik2").innerHTML = "Prędkość wiatru: " + data.current.wind_kph + "km/h";
+          document.getElementById("Wynik3").innerHTML = "http" + data.current.icon;
         })
         .catch(error => {
-            Wynik.innerHTML=("Błąd", error);
+          document.getElementById("Wynik1").innerHTML="Błąd" + error;
         });
     }
