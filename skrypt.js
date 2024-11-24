bbbc3cba0df6f59caec0efb11f78500ef2abace0
Gnoblindo <szymon.przybyla2010@gmail.com>
2024-11-24 17:52:09 +0100

    system pogoda dla zabrza

--- skrypt.js
+++ skrypt.js
@@ -1,1 +1,22 @@
-document.write("Istnieje .js");
+var przy1 = document.getElementById('Pogoda');
+var Wynik = document.getElementById("Wynik")
+przy1.addEventListener("click", function(){
+	fetch('http://api.weatherapi.com/v1/search.json?key=<12c455b685114ca09cd163336242411>&q=Zabrze', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Błąd sieci');
+          }
+          return response.json();
+        })
+        .then(data => {
+          Wynik.innerHTML(data);
+        })
+        .catch(error => {
+            Wynik.innerHTML=("Błąd", error);
+        });
+});
