7329835ec43e908297e08d28b9e60b094ecf2166
Gnoblindo <szymon.przybyla2010@gmail.com>
2024-11-28 17:15:09 +0100

    Poprawne szukanie miast i grafika

--- skrypt.js
+++ skrypt.js
@@ -1,5 +1,11 @@
 function fetchData() {
-	fetch('http://api.weatherapi.com/v1/current.json?key=12c455b685114ca09cd163336242411&q=Zabrze&lang=pl', {
+  const miasto = document.getElementById("miasto").value;
+  if(!miasto)  {
+    alert("Wpisz miasto");
+    return;
+  }
+
+	fetch('http://api.weatherapi.com/v1/current.json?key=12c455b685114ca09cd163336242411&q=' + miasto +'&lang=pl', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
@@ -15,7 +21,10 @@ function fetchData() {
           console.log(JSON.stringify(data));
           document.getElementById("Wynik1").innerHTML = "Temperatura: " + data.current.temp_c + "°C";
           document.getElementById("Wynik2").innerHTML = "Prędkość wiatru: " + data.current.wind_kph + "km/h";
-          document.getElementById("Wynik3").innerHTML = "http" + data.current.icon;
+          document.getElementById("Text").innerHTML = "Niebo:" + data.current.condition.text;
+          //debugger;
+          var Wynik3 = document.getElementById("Wynik3");
+          Wynik3.src = "https:" + data.current.condition.icon;
         })
         .catch(error => {
           document.getElementById("Wynik1").innerHTML="Błąd" + error;
