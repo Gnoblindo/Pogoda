34aa840c506c24e66f92a1dcf29406791c6deaae
Gnoblindo <szymon.przybyla2010@gmail.com>
2024-12-01 20:56:31 +0100

    Początki działań

--- skrypt.js
+++ skrypt.js
@@ -1,10 +1,29 @@
-function fetchData() {
+function fetchData1(){
   const miasto = document.getElementById("miasto").value;
-  if(!miasto)  {
-    alert("Wpisz miasto");
-    return;
-  }
+  fetch('http://api.weatherapi.com/v1/search.json?key=12c455b685114ca09cd163336242411&q=' + miasto +'&lang=pl', {
+    method: 'GET',
+    heathers: {
+      'Content-type': 'application/json'
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Błąd sieci');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(JSON.stringify(data));
+    if(data[0] = length0){
 
+    }
+  })
+  .catch(error => {
+    document.getElementById("coSzukam").innerHTML="Błąd" + error;
+  })
+}
+function fetchData2() {
+  const miasto = document.getElementById("miasto").value;
 	fetch('http://api.weatherapi.com/v1/current.json?key=12c455b685114ca09cd163336242411&q=' + miasto +'&lang=pl', {
         method: 'GET',
         headers: {
@@ -19,14 +38,14 @@ function fetchData() {
         })
         .then(data => {
           console.log(JSON.stringify(data));
-          document.getElementById("Wynik1").innerHTML = "Temperatura: " + data.current.temp_c + "°C";
-          document.getElementById("Wynik2").innerHTML = "Prędkość wiatru: " + data.current.wind_kph + "km/h";
-          document.getElementById("Text").innerHTML = "Niebo:" + data.current.condition.text;
+          document.getElementById("wynik1").innerHTML = "Temperatura: " + data.current.temp_c + "°C";
+          document.getElementById("wynik2").innerHTML = "Prędkość wiatru: " + data.current.wind_kph + "km/h";
+          document.getElementById("text").innerHTML = "Niebo:" + data.current.condition.text;
           //debugger;
-          var Wynik3 = document.getElementById("Wynik3");
-          Wynik3.src = "https:" + data.current.condition.icon;
+          var wynik3 = document.getElementById("wynik3");
+          wynik3.src = "https:" + data.current.condition.icon;
         })
         .catch(error => {
-          document.getElementById("Wynik1").innerHTML="Błąd" + error;
+          document.getElementById("wynik1").innerHTML="Błąd" + error;
         });
     }
